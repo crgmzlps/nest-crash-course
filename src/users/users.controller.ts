75d870ec4901eae02fe9fec041198d03bbc3742c
Carlo Roberto <crgmzlps@gmail.com>
2021-07-19 20:39:34 -0300

    dependency injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
-  async getUsers() {
-    return [{ id: 1 }];
+  getUsers() {
+    return this.usersService.findAll();
   }
+
   @Get(':id')
-  async getUserById(@Param('id', ParseIntPipe) id: number) {
-    return { id };
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findById(id);
+  }
+
+  @Post()
+  createUser(@Body('name') name: string) {
+    return this.usersService.create({ name });
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    { id: 1, name: 'john doe' },
+    { id: 2, name: 'jane doe' },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findById(id: number) {
+    return this.users.find((u) => u.id === id);
+  }
+
+  create(data) {
+    const newUser = { id: this.users.length + 1, ...data };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
