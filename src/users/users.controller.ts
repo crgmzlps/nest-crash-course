02f0fd7185e3ad3f9ca855b584719bd323a6b693
Carlo Roberto <crgmzlps@gmail.com>
2021-07-19 20:25:00 -0300

    parsing url params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
   async getUsers() {
     return [{ id: 1 }];
+  }
+  @Get(':id')
+  async getUserById(@Param('id', ParseIntPipe) id: number) {
+    return { id };
   }
 }
